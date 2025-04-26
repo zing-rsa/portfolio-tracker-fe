1cdbc8a8e26b1772d95a1cb4bb75dff11b1379a9
Rob <kritz.rob@gmail.com>
2025-04-26 09:15:54 +0200

    more fixes

--- src/app/api/v1/balances/historic/route.ts
+++ src/app/api/v1/balances/historic/route.ts
@@ -1,8 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 import { API_HOST, API_KEY } from "../../../constants"
 
-export async function GET(req: NextRequest) {
+export async function GET() {
 
   const response = await fetch(`${API_HOST}/api/v1/balances/historic`, {
     method: "GET",
